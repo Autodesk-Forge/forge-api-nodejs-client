8c3fcfb60349938764b138b5b461c742237144b3
Greg Rashkevitch <rashkevitch@gmail.com>
2017-01-10 15:46:56 +0200

    Bring back convertToType

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -400,6 +400,76 @@ module.exports = (function() {
   };
 
   /**
+   * Parses an ISO-8601 string representation of a date value.
+   * @param {String} str The date value as a string.
+   * @returns {Date} The parsed date object.
+   */
+  exports.parseDate = function(str) {
+      return new Date(str.replace(/T/i, ' '));
+  };
+
+  /**
+   * Converts a value to the specified type.
+   * @param {(String|Object)} data The data to convert, as a string or object.
+   * @param {(String|Array.<String>|Object.<String, Object>|Function)} type The type to return. Pass a string for simple types
+   * or the constructor function for a complex type. Pass an array containing the type name to return an array of that type. To
+   * return an object, pass an object with one property whose name is the key type and whose value is the corresponding value type:
+   * all properties on <code>data<code> will be converted to this type.
+   * @returns {Object} An instance of the specified type.
+   */
+  exports.convertToType = function(data, type) {
+        switch (type) {
+            case 'Boolean':
+                return Boolean(data);
+            case 'Integer':
+                return parseInt(data, 10);
+            case 'Number':
+                return parseFloat(data);
+            case 'String':
+                return String(data);
+            case 'Date':
+                return this.parseDate(String(data));
+            default:
+                if (type === Object) {
+                    // generic object, return directly
+                    return data;
+                } else if (typeof type === 'function') {
+                    // for model type like: User
+                    return type.constructFromObject(data);
+                } else if (Array.isArray(type)) {
+                    // for array type like: ['String']
+                    var itemType = type[0];
+                    return data.map(function(item) {
+                        return exports.convertToType(item, itemType);
+                    });
+                } else if (typeof type === 'object') {
+                    // for plain object type like: {'String': 'Integer'}
+                    var keyType, valueType;
+                    for (var k in type) {
+                        if (type.hasOwnProperty(k)) {
+                            keyType = k;
+                            valueType = type[k];
+                            break;
+                        }
+                    }
+                    var result = {};
+                    for (var j in data) {
+                        if (data.hasOwnProperty(j)) {
+                            var key = exports.convertToType(j, keyType);
+                            result[key] = exports.convertToType(data[j], valueType);
+                        }
+                    }
+                    return result;
+                } else {
+                    // for unknown type, return the data directly
+                    return data;
+                }
+        }
+  };
+
+
+
+  /**
    * The default API client implementation.
    * @type {module:ApiClient}
    */
