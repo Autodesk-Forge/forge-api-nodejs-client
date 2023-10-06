4b5962a68b78d589191ca550061ae785190b0d29
cyrille <cyrille.fauvel@autodesk.com>
2023-10-06 14:23:09 +0200

    refix for 101

--- src/api/ObjectsApi.js
+++ src/api/ObjectsApi.js
@@ -372,8 +372,7 @@ module.exports = (function () {
 			const contentTypes = ['application/json'];
 			const accepts = opts.accepts || ['application/octet-stream'];
 			const returnType = Object;
-			const responseType = 'arraybuffer';
-
+			
 			return (this.apiClient.callApi(
 				'/oss/v2/buckets/{bucketKey}/objects/{objectKey}', 'GET',
 				pathParams, queryParams, headerParams, formParams, postBody,
